419d054d19a778a5c9b3a9b42a5c82ed8574dbb4
Vitaly <vitalysavchuk2312@gmail.com>
2019-06-05 19:08:21 +0300

    almost finished calculation logic; last problem: prior division and multiplication

--- script.js
+++ script.js
@@ -1,52 +1,80 @@
 const main = (function() {
-    return {
-        currentValue: '',
-        operators: [],
-        selectors: {
-            calcSymbolsArray: document.querySelectorAll('.calc__row-item:not(.calc__row-item-clear):not(.calc__row-item-evaluate)'),
-            calcVisibleValue: document.querySelector('.calc__result-value'),
-            calcOperators: document.querySelectorAll('.calc__row-item-operator'),
-            calcEvaluate: document.querySelector('.calc__row-item-evaluate')
-        },
-        showOperation() {
-            this.selectors.calcSymbolsArray.forEach(item => {
-                item.addEventListener('click', (e) => {
-                    this.currentValue += e.target.innerHTML;
-                    this.selectors.calcVisibleValue.innerHTML = this.currentValue;
-                })
+    let currentValue = '';
+    let currentResult = [];
+    let operators = [];
+    const selectors = {
+        calcSymbolsArray: document.querySelectorAll('.calc__row-item:not(.calc__row-item-clear):not(.calc__row-item-evaluate)'),
+        calcVisibleValue: document.querySelector('.calc__result-value'),
+        calcOperators: document.querySelectorAll('.calc__row-item-operator'),
+        calcEvaluate: document.querySelector('.calc__row-item-evaluate')
+    };
+    function showOperation() {
+        selectors.calcSymbolsArray.forEach(item => {
+            item.addEventListener('click', (e) => {
+                currentValue += e.target.innerHTML;
+                selectors.calcVisibleValue.innerHTML = currentValue;
             })
-        },
-        calculate() {
-            this.selectors.calcEvaluate.addEventListener('click', (e) => {
-                const onlyDigits = this.currentValue.split(/[*+-/]/);
-                const result = operators.forEach((item, index) => {
+        })
+    }
+    function evaluate() {
+        selectors.calcEvaluate.addEventListener('click', (e) => {
+            let result = currentResult[0];
+            const test = operators.filter(item => item !== "*" && item !== "/");
+            test.forEach((item, index) => {
+                if (currentResult[index + 1] !== undefined) {
                     switch(item) {
-                        case '+':
-                            onlyDigits[index]
+                        case "+": 
+                            result += currentResult[index + 1]; 
+                            break;
+                        case "-":
+                            result -= currentResult[index + 1]; 
+                            break;
+                        default:
+                            console.log('not correct operator');
+                            break;
                     }
-                });
+                }
             });
-        },
-        clear() {
-            document.querySelector('.calc__row-item-clear').addEventListener('click', () => {
-                this.currentValue = [];
-                this.selectors.calcVisibleValue.innerHTML = 0;
+        });
+    };
+    function clear() {
+        document.querySelector('.calc__row-item-clear').addEventListener('click', () => {
+            currentValue = [];
+            operators = [];
+            selectors.calcVisibleValue.innerHTML = 0;
+        });
+    };
+    function operate() {
+        selectors.calcOperators.forEach(item => {
+            item.addEventListener('click', (e) => {
+                operators.push(e.target.innerHTML);
+                calculatePriorOperators();
             });
-        },
-        test() {
-            this.selectors.calcOperators.forEach(item => {
-                item.addEventListener('click', (e) => {
-                    this.operators.push(e.target.innerHTML);
-                })
-            })
-        },
-        init() {
-            this.showOperation();
-            this.calculate();
-            this.test();
-            this.clear();
+        })
+    };
+    function getOnlyDigits() {
+        // hardcode with filter!!!
+        return currentValue.split(/[*+-/]/).filter(item => item !== "").map(item => Number(item));
+    }
+    // multiply and divide
+    function calculatePriorOperators() {
+        const onlyDigits = getOnlyDigits();
+        if (currentResult.length < 1) {
+            currentResult = onlyDigits;
+        }
+        if (operators.length > 1 && operators[operators.length - 2] === "*") {
+            const result = onlyDigits[operators.length - 2] * onlyDigits[operators.length - 1];
+            currentResult.splice(operators.length - 2, 2, result);
+            console.log(currentResult);
         }
     }
+    function init() {
+        showOperation();
+        evaluate();
+        operate();
+        clear();
+    }
+    return { init }
 })();
 
 main.init(); 
