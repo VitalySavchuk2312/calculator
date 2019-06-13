89093641093dfd3e44b4eb5b4ffdace2ace366fa
Vitaly <vitalysavchuk2312@gmail.com>
2019-06-13 13:10:07 +0300

    combined addition and unitary multiplication

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const main = (function() {
     let currentValue = '';
-    let currentResult = [];
     let operators = [];
     const selectors = {
         calcSymbolsArray: document.querySelectorAll('.calc__row-item:not(.calc__row-item-clear):not(.calc__row-item-evaluate)'),
         calcVisibleValue: document.querySelector('.calc__result-value'),
         calcOperators: document.querySelectorAll('.calc__row-item-operator'),
+        calcDigits: document.querySelectorAll('.calc__row-item--digit'), 
         calcEvaluate: document.querySelector('.calc__row-item-evaluate')
     };
     function showOperation() {
@@ -18,23 +18,42 @@ const main = (function() {
     }
     function evaluate() {
         selectors.calcEvaluate.addEventListener('click', (e) => {
-            let result = currentResult[0];
-            const test = operators.filter(item => item !== "*" && item !== "/");
-            test.forEach((item, index) => {
-                if (currentResult[index + 1] !== undefined) {
+            // calculatePriorOperators();
+            const onlyDigits = getOnlyDigits();
+            let result = onlyDigits[0];
+            operators.forEach((item, index) => {
+                if (onlyDigits[index + 1] !== undefined) {
                     switch(item) {
                         case "+": 
-                            result += currentResult[index + 1]; 
+                            if (operators[index + 1] !== "*" && operators[index + 1] !== "/") {
+                                result += onlyDigits[index + 1];
+                            } else if(operators[index + 1] === "*") {
+                                result += (onlyDigits[index + 1] * onlyDigits[index + 2]);
+                                onlyDigits.splice(index + 1, 2, result);
+                                operators.splice(index + 1, 1);
+                            } else if(operators[index + 1] === "/") {
+                                result += (onlyDigits[index + 1] / onlyDigits[index + 2]);
+                                onlyDigits.splice(index + 1, 2, result);
+                                operators.splice(index + 1, 1);
+                            }
                             break;
                         case "-":
-                            result -= currentResult[index + 1]; 
+                            result -= onlyDigits[index + 1]; 
+                            break;
+                        case "*":
+                            result *= onlyDigits[index + 1]; 
+                            break;
+                        case "/":
+                            result /= onlyDigits[index + 1]; 
                             break;
                         default:
                             console.log('not correct operator');
                             break;
                     }
                 }
+                console.log(result);
             });
+            // selectors.calcVisibleValue.innerHTML = result;
         });
     };
     function clear() {
@@ -48,25 +67,12 @@ const main = (function() {
         selectors.calcOperators.forEach(item => {
             item.addEventListener('click', (e) => {
                 operators.push(e.target.innerHTML);
-                calculatePriorOperators();
             });
-        })
+        });
     };
     function getOnlyDigits() {
         // hardcode with filter!!!
         return currentValue.split(/[*+-/]/).filter(item => item !== "").map(item => Number(item));
-    }
-    // multiply and divide
-    function calculatePriorOperators() {
-        const onlyDigits = getOnlyDigits();
-        if (currentResult.length < 1) {
-            currentResult = onlyDigits;
-        }
-        if (operators.length > 1 && operators[operators.length - 2] === "*") {
-            const result = onlyDigits[operators.length - 2] * onlyDigits[operators.length - 1];
-            currentResult.splice(operators.length - 2, 2, result);
-            console.log(currentResult);
-        }
     }
     function init() {
         showOperation();
